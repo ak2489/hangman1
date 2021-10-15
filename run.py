7c361a58ce783f4be02d25a5f62e9b4f45bdd23e
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2021-10-15 15:22:02 +0000

    created user input to get guesses and add letters to word_letters if in word or add to used letters if not in word

--- run.py
+++ run.py
@@ -16,3 +16,18 @@ def hangman():
     word_letters = set(word)  # letters in word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
+
+    # getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+
+    else:
+        print('Invalid character. Please try again.')
+
+print(user_letter)
