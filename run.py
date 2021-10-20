8046b565d4b09c84ae9daa830a1c4b4dc8d51d5f
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2021-10-20 16:04:26 +0000

    created function of lives to limit number of guesses by user and end game if lives go to 0 or if word guessed.

--- run.py
+++ run.py
@@ -19,22 +19,38 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
+    lives = 7
+
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
-        print('You have used there letters: ', ' '.join(used_letters))
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
+        # what the current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
 
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1 # takes away a life if wrong
+                print('Letter is not in word.')
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again.')
+
+    # gets here when len(word_letters) == 0 or when lives == 0
+    if lives == 0:
+        print('You died, sorry. The word was', word)
     else:
-        print('Invalid character. Please try again.')
+        print('You have guessed the word', word, '!!')
 
-    # gets here when len(word_letters) == 0
 
 hangman()
